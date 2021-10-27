277ddbc767e5f823f893e96bac758c05a8e3efe7
Ricky Zenati <b00629577@essec.edu>
2021-10-28 01:52:29 +0200

    avancées

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,15 +1,30 @@
 class FlatsController < ApplicationController
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
   end
 
   def show
+    @flat = Flat.find(params[:id])
   end
 
   def index
-    @flats = Flat.all
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name LIKE ?","%#{@query}%")
+      # Preventing SQL Injection and Database error for
+      # unknown characters
+    else
+      @flats = Flat.all
+    end
   end
 
   def update
@@ -19,5 +34,11 @@ class FlatsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
   end
 end
